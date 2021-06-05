7470e5bc65ce83335e16d7f3103a4ef9b5d77250
chau minh duc <19521371@gm.uit.edu.vn>
2021-06-05 22:56:53 +0700

    Done base functions

---
+++ NetTools/UserControls/Network/UCWhoIs.Designer.cs
@@ -0,0 +1,151 @@
+﻿
+namespace NetTools.UserControls.Network
+{
+    partial class UCWhoIs
+    {
+        /// <summary> 
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary> 
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Component Designer generated code
+
+        /// <summary> 
+        /// Required method for Designer support - do not modify 
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.panel2 = new System.Windows.Forms.Panel();
+            this.panel3 = new System.Windows.Forms.Panel();
+            this.buttonGo = new System.Windows.Forms.Button();
+            this.textIPAddress = new System.Windows.Forms.TextBox();
+            this.label2 = new System.Windows.Forms.Label();
+            this.labelIPAddress = new System.Windows.Forms.Label();
+            this.richTextInfomation = new System.Windows.Forms.RichTextBox();
+            this.panel2.SuspendLayout();
+            this.panel3.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // panel2
+            // 
+            this.panel2.Controls.Add(this.buttonGo);
+            this.panel2.Controls.Add(this.textIPAddress);
+            this.panel2.Controls.Add(this.label2);
+            this.panel2.Controls.Add(this.labelIPAddress);
+            this.panel2.Dock = System.Windows.Forms.DockStyle.Top;
+            this.panel2.Location = new System.Drawing.Point(0, 0);
+            this.panel2.Name = "panel2";
+            this.panel2.Size = new System.Drawing.Size(980, 113);
+            this.panel2.TabIndex = 1;
+            // 
+            // panel3
+            // 
+            this.panel3.Controls.Add(this.richTextInfomation);
+            this.panel3.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.panel3.Location = new System.Drawing.Point(0, 113);
+            this.panel3.Name = "panel3";
+            this.panel3.Padding = new System.Windows.Forms.Padding(10, 0, 10, 10);
+            this.panel3.Size = new System.Drawing.Size(980, 716);
+            this.panel3.TabIndex = 2;
+            // 
+            // buttonGo
+            // 
+            this.buttonGo.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Right)));
+            this.buttonGo.BackColor = System.Drawing.Color.WhiteSmoke;
+            this.buttonGo.Cursor = System.Windows.Forms.Cursors.Arrow;
+            this.buttonGo.FlatAppearance.BorderSize = 0;
+            this.buttonGo.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.buttonGo.Font = new System.Drawing.Font("Microsoft Sans Serif", 10.2F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.buttonGo.Location = new System.Drawing.Point(773, 24);
+            this.buttonGo.Name = "buttonGo";
+            this.buttonGo.Size = new System.Drawing.Size(75, 37);
+            this.buttonGo.TabIndex = 6;
+            this.buttonGo.Text = "Go";
+            this.buttonGo.UseVisualStyleBackColor = false;
+            this.buttonGo.Click += new System.EventHandler(this.buttonGo_Click);
+            // 
+            // textIPAddress
+            // 
+            this.textIPAddress.Anchor = ((System.Windows.Forms.AnchorStyles)(((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Left) 
+            | System.Windows.Forms.AnchorStyles.Right)));
+            this.textIPAddress.Cursor = System.Windows.Forms.Cursors.IBeam;
+            this.textIPAddress.Font = new System.Drawing.Font("Microsoft Sans Serif", 10.2F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.textIPAddress.Location = new System.Drawing.Point(261, 29);
+            this.textIPAddress.MinimumSize = new System.Drawing.Size(300, 27);
+            this.textIPAddress.Name = "textIPAddress";
+            this.textIPAddress.Size = new System.Drawing.Size(481, 27);
+            this.textIPAddress.TabIndex = 5;
+            // 
+            // label2
+            // 
+            this.label2.Anchor = ((System.Windows.Forms.AnchorStyles)((((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Bottom) 
+            | System.Windows.Forms.AnchorStyles.Left) 
+            | System.Windows.Forms.AnchorStyles.Right)));
+            this.label2.AutoSize = true;
+            this.label2.Font = new System.Drawing.Font("Microsoft Sans Serif", 10.2F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label2.Location = new System.Drawing.Point(257, 69);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(413, 20);
+            this.label2.TabIndex = 3;
+            this.label2.Text = "WhoIs information provided by whois.whoisxmlapi.com";
+            // 
+            // labelIPAddress
+            // 
+            this.labelIPAddress.AutoSize = true;
+            this.labelIPAddress.Font = new System.Drawing.Font("Microsoft Sans Serif", 10.2F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.labelIPAddress.Location = new System.Drawing.Point(90, 32);
+            this.labelIPAddress.Name = "labelIPAddress";
+            this.labelIPAddress.Size = new System.Drawing.Size(153, 20);
+            this.labelIPAddress.TabIndex = 4;
+            this.labelIPAddress.Text = "Domain name or IP";
+            // 
+            // richTextInfomation
+            // 
+            this.richTextInfomation.BorderStyle = System.Windows.Forms.BorderStyle.FixedSingle;
+            this.richTextInfomation.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.richTextInfomation.Location = new System.Drawing.Point(10, 0);
+            this.richTextInfomation.Name = "richTextInfomation";
+            this.richTextInfomation.ReadOnly = true;
+            this.richTextInfomation.Size = new System.Drawing.Size(960, 706);
+            this.richTextInfomation.TabIndex = 0;
+            this.richTextInfomation.Text = "";
+            // 
+            // UCWhoIs
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(120F, 120F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Dpi;
+            this.Controls.Add(this.panel3);
+            this.Controls.Add(this.panel2);
+            this.Name = "UCWhoIs";
+            this.Size = new System.Drawing.Size(980, 829);
+            this.panel2.ResumeLayout(false);
+            this.panel2.PerformLayout();
+            this.panel3.ResumeLayout(false);
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+        private System.Windows.Forms.Panel panel2;
+        private System.Windows.Forms.Panel panel3;
+        private System.Windows.Forms.Button buttonGo;
+        private System.Windows.Forms.TextBox textIPAddress;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label labelIPAddress;
+        private System.Windows.Forms.RichTextBox richTextInfomation;
+    }
+}
---
+++ NetTools/UserControls/Network/UCWhoIs.cs
@@ -0,0 +1,80 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.Xml;
+using System.Net;
+using System.IO;
+
+namespace NetTools.UserControls.Network
+{
+    public partial class UCWhoIs : UserControl
+    {
+        // Constructor
+        public UCWhoIs()
+        {
+            InitializeComponent();
+        }
+
+        #region Methods
+        private string GetHTTPRequest(string ipAddress)
+        {
+            string URL = $"https://www.whoisxmlapi.com/whoisserver/WhoisService?apiKey=at_aVzDK57jaecAL81LDLNTpFpZobX4B&domainName={ipAddress}";
+            WebRequest webRequest = WebRequest.Create(URL);
+            webRequest.Credentials = CredentialCache.DefaultCredentials;
+            WebResponse webResponse = webRequest.GetResponse();
+            Stream stream = webResponse.GetResponseStream();
+            StreamReader streamReader = new StreamReader(stream);
+            string httpResponse = streamReader.ReadToEnd();
+            stream.Close();
+            webResponse.Close();
+            return httpResponse;
+        }
+
+        private bool CheckQueryReponse(XmlDocument xDoc)
+        {
+            if (xDoc.SelectSingleNode("//ErrorMessage") != null)
+                return false;
+            return true;
+        }
+        #endregion
+
+        #region User interaction
+        private void buttonGo_Click(object sender, EventArgs e)
+        {
+            if (textIPAddress.Text != string.Empty)
+            {
+                string xmlRespone = GetHTTPRequest(textIPAddress.Text);
+                XmlDocument xDoc = new XmlDocument();
+                xDoc.LoadXml(xmlRespone);
+                richTextInfomation.Clear();
+                try
+                {
+                    if (CheckQueryReponse(xDoc))
+                    {
+                        richTextInfomation.Text = xDoc.SelectSingleNode("./WhoisRecord[1]/rawText[1]").InnerText;
+
+                    }
+                    else
+                    {
+                        string errorCode = $"Error code: {xDoc.SelectSingleNode("//errorCode").InnerText}";
+                        string msg = $"Message: {xDoc.SelectSingleNode("//msg").InnerText}";
+                        richTextInfomation.Text += errorCode;
+                        richTextInfomation.Text += "\n";
+                        richTextInfomation.Text += msg;
+                    }
+                }
+                catch (Exception ex)
+                {
+                    richTextInfomation.Text = ex.Message;
+                }
+            }
+        }
+        #endregion
+    }
+}
