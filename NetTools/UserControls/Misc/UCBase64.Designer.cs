9ceca637bfa6d4c5f5f7c66a2e95c4f26fe8664c
Đức Châu Minh <19521371@ms.uit.edu.vn>
2021-06-24 23:44:15 +0700

    Design UI

--- NetTools/UserControls/Misc/UCBase64.Designer.cs
+++ NetTools/UserControls/Misc/UCBase64.Designer.cs
@@ -29,19 +29,330 @@ namespace NetTools.UserControls.Misc
         /// </summary>
         private void InitializeComponent()
         {
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(UCBase64));
+            this.panelInput = new System.Windows.Forms.Panel();
+            this.panelOutput = new System.Windows.Forms.Panel();
+            this.panelChecksum = new System.Windows.Forms.Panel();
+            this.buttonOpenFile = new System.Windows.Forms.Button();
+            this.label1 = new System.Windows.Forms.Label();
+            this.comboxCharacterEncoding = new System.Windows.Forms.ComboBox();
+            this.label2 = new System.Windows.Forms.Label();
+            this.richtextInput = new System.Windows.Forms.RichTextBox();
+            this.button1 = new System.Windows.Forms.Button();
+            this.button2 = new System.Windows.Forms.Button();
+            this.label3 = new System.Windows.Forms.Label();
+            this.richTextBox1 = new System.Windows.Forms.RichTextBox();
+            this.label4 = new System.Windows.Forms.Label();
+            this.richTextBox2 = new System.Windows.Forms.RichTextBox();
+            this.label5 = new System.Windows.Forms.Label();
+            this.richTextBox3 = new System.Windows.Forms.RichTextBox();
+            this.label6 = new System.Windows.Forms.Label();
+            this.comboBox1 = new System.Windows.Forms.ComboBox();
+            this.comboBox2 = new System.Windows.Forms.ComboBox();
+            this.richTextBox4 = new System.Windows.Forms.RichTextBox();
+            this.panelInput.SuspendLayout();
+            this.panelOutput.SuspendLayout();
+            this.panelChecksum.SuspendLayout();
             this.SuspendLayout();
+            // 
+            // panelInput
+            // 
+            this.panelInput.Controls.Add(this.richtextInput);
+            this.panelInput.Controls.Add(this.label2);
+            this.panelInput.Controls.Add(this.comboxCharacterEncoding);
+            this.panelInput.Controls.Add(this.label1);
+            this.panelInput.Controls.Add(this.button2);
+            this.panelInput.Controls.Add(this.button1);
+            this.panelInput.Controls.Add(this.buttonOpenFile);
+            this.panelInput.Dock = System.Windows.Forms.DockStyle.Top;
+            this.panelInput.Font = new System.Drawing.Font("Microsoft Sans Serif", 10.2F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.panelInput.Location = new System.Drawing.Point(0, 0);
+            this.panelInput.Name = "panelInput";
+            this.panelInput.Padding = new System.Windows.Forms.Padding(50, 0, 50, 0);
+            this.panelInput.Size = new System.Drawing.Size(980, 297);
+            this.panelInput.TabIndex = 0;
+            // 
+            // panelOutput
+            // 
+            this.panelOutput.Controls.Add(this.richTextBox3);
+            this.panelOutput.Controls.Add(this.label5);
+            this.panelOutput.Controls.Add(this.richTextBox2);
+            this.panelOutput.Controls.Add(this.label4);
+            this.panelOutput.Controls.Add(this.richTextBox1);
+            this.panelOutput.Controls.Add(this.label3);
+            this.panelOutput.Dock = System.Windows.Forms.DockStyle.Top;
+            this.panelOutput.Font = new System.Drawing.Font("Microsoft Sans Serif", 10.2F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.panelOutput.Location = new System.Drawing.Point(0, 297);
+            this.panelOutput.Name = "panelOutput";
+            this.panelOutput.Size = new System.Drawing.Size(980, 424);
+            this.panelOutput.TabIndex = 1;
+            // 
+            // panelChecksum
+            // 
+            this.panelChecksum.Controls.Add(this.richTextBox4);
+            this.panelChecksum.Controls.Add(this.comboBox2);
+            this.panelChecksum.Controls.Add(this.comboBox1);
+            this.panelChecksum.Controls.Add(this.label6);
+            this.panelChecksum.Dock = System.Windows.Forms.DockStyle.Top;
+            this.panelChecksum.Font = new System.Drawing.Font("Microsoft Sans Serif", 10.2F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.panelChecksum.Location = new System.Drawing.Point(0, 721);
+            this.panelChecksum.Name = "panelChecksum";
+            this.panelChecksum.Size = new System.Drawing.Size(980, 108);
+            this.panelChecksum.TabIndex = 2;
+            // 
+            // buttonOpenFile
+            // 
+            this.buttonOpenFile.FlatAppearance.BorderSize = 0;
+            this.buttonOpenFile.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.buttonOpenFile.Font = new System.Drawing.Font("Microsoft Sans Serif", 10.2F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.buttonOpenFile.Image = ((System.Drawing.Image)(resources.GetObject("buttonOpenFile.Image")));
+            this.buttonOpenFile.ImageAlign = System.Drawing.ContentAlignment.MiddleLeft;
+            this.buttonOpenFile.Location = new System.Drawing.Point(48, 26);
+            this.buttonOpenFile.Name = "buttonOpenFile";
+            this.buttonOpenFile.Padding = new System.Windows.Forms.Padding(0, 0, 3, 0);
+            this.buttonOpenFile.Size = new System.Drawing.Size(138, 37);
+            this.buttonOpenFile.TabIndex = 0;
+            this.buttonOpenFile.Text = "       Open File";
+            this.buttonOpenFile.UseVisualStyleBackColor = true;
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Font = new System.Drawing.Font("Microsoft Sans Serif", 10.2F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label1.Location = new System.Drawing.Point(44, 224);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(155, 20);
+            this.label1.TabIndex = 1;
+            this.label1.Text = "Character encoding";
+            // 
+            // comboxCharacterEncoding
+            // 
+            this.comboxCharacterEncoding.BackColor = System.Drawing.Color.WhiteSmoke;
+            this.comboxCharacterEncoding.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.comboxCharacterEncoding.Font = new System.Drawing.Font("Microsoft Sans Serif", 10.2F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.comboxCharacterEncoding.FormattingEnabled = true;
+            this.comboxCharacterEncoding.Items.AddRange(new object[] {
+            "ASCII",
+            "UTF8",
+            "Unicode"});
+            this.comboxCharacterEncoding.Location = new System.Drawing.Point(48, 247);
+            this.comboxCharacterEncoding.Name = "comboxCharacterEncoding";
+            this.comboxCharacterEncoding.Size = new System.Drawing.Size(152, 28);
+            this.comboxCharacterEncoding.TabIndex = 2;
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(44, 76);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(82, 20);
+            this.label2.TabIndex = 3;
+            this.label2.Text = "Text input";
+            // 
+            // richtextInput
+            // 
+            this.richtextInput.Anchor = ((System.Windows.Forms.AnchorStyles)(((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Left) 
+            | System.Windows.Forms.AnchorStyles.Right)));
+            this.richtextInput.BackColor = System.Drawing.Color.WhiteSmoke;
+            this.richtextInput.BorderStyle = System.Windows.Forms.BorderStyle.None;
+            this.richtextInput.Location = new System.Drawing.Point(48, 99);
+            this.richtextInput.Name = "richtextInput";
+            this.richtextInput.Size = new System.Drawing.Size(882, 100);
+            this.richtextInput.TabIndex = 4;
+            this.richtextInput.Text = "";
+            // 
+            // button1
+            // 
+            this.button1.FlatAppearance.BorderSize = 0;
+            this.button1.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.button1.Font = new System.Drawing.Font("Microsoft Sans Serif", 10.2F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.button1.Image = ((System.Drawing.Image)(resources.GetObject("button1.Image")));
+            this.button1.ImageAlign = System.Drawing.ContentAlignment.MiddleLeft;
+            this.button1.Location = new System.Drawing.Point(234, 242);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(125, 37);
+            this.button1.TabIndex = 0;
+            this.button1.Text = "       Convert";
+            this.button1.TextAlign = System.Drawing.ContentAlignment.MiddleLeft;
+            this.button1.UseVisualStyleBackColor = true;
+            // 
+            // button2
+            // 
+            this.button2.FlatAppearance.BorderSize = 0;
+            this.button2.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.button2.Font = new System.Drawing.Font("Microsoft Sans Serif", 10.2F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.button2.Image = ((System.Drawing.Image)(resources.GetObject("button2.Image")));
+            this.button2.ImageAlign = System.Drawing.ContentAlignment.MiddleLeft;
+            this.button2.Location = new System.Drawing.Point(365, 242);
+            this.button2.Name = "button2";
+            this.button2.Size = new System.Drawing.Size(106, 37);
+            this.button2.TabIndex = 0;
+            this.button2.Text = "       Reset";
+            this.button2.TextAlign = System.Drawing.ContentAlignment.MiddleLeft;
+            this.button2.UseVisualStyleBackColor = true;
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Font = new System.Drawing.Font("Microsoft Sans Serif", 10.2F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label3.Location = new System.Drawing.Point(44, 3);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(96, 20);
+            this.label3.TabIndex = 1;
+            this.label3.Text = "Hex (bytes)";
+            // 
+            // richTextBox1
+            // 
+            this.richTextBox1.Anchor = ((System.Windows.Forms.AnchorStyles)(((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Left) 
+            | System.Windows.Forms.AnchorStyles.Right)));
+            this.richTextBox1.BackColor = System.Drawing.Color.WhiteSmoke;
+            this.richTextBox1.BorderStyle = System.Windows.Forms.BorderStyle.None;
+            this.richTextBox1.Location = new System.Drawing.Point(48, 26);
+            this.richTextBox1.Name = "richTextBox1";
+            this.richTextBox1.Size = new System.Drawing.Size(882, 100);
+            this.richTextBox1.TabIndex = 5;
+            this.richTextBox1.Text = "";
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Font = new System.Drawing.Font("Microsoft Sans Serif", 10.2F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label4.Location = new System.Drawing.Point(44, 142);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(114, 20);
+            this.label4.TabIndex = 1;
+            this.label4.Text = "Binary (bytes)";
+            // 
+            // richTextBox2
+            // 
+            this.richTextBox2.Anchor = ((System.Windows.Forms.AnchorStyles)(((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Left) 
+            | System.Windows.Forms.AnchorStyles.Right)));
+            this.richTextBox2.BackColor = System.Drawing.Color.WhiteSmoke;
+            this.richTextBox2.BorderStyle = System.Windows.Forms.BorderStyle.None;
+            this.richTextBox2.Location = new System.Drawing.Point(48, 165);
+            this.richTextBox2.Name = "richTextBox2";
+            this.richTextBox2.Size = new System.Drawing.Size(882, 100);
+            this.richTextBox2.TabIndex = 5;
+            this.richTextBox2.Text = "";
+            // 
+            // label5
+            // 
+            this.label5.AutoSize = true;
+            this.label5.Font = new System.Drawing.Font("Microsoft Sans Serif", 10.2F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label5.Location = new System.Drawing.Point(44, 284);
+            this.label5.Name = "label5";
+            this.label5.Size = new System.Drawing.Size(123, 20);
+            this.label5.TabIndex = 1;
+            this.label5.Text = "Base64 (bytes)";
+            // 
+            // richTextBox3
+            // 
+            this.richTextBox3.Anchor = ((System.Windows.Forms.AnchorStyles)(((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Left) 
+            | System.Windows.Forms.AnchorStyles.Right)));
+            this.richTextBox3.BackColor = System.Drawing.Color.WhiteSmoke;
+            this.richTextBox3.BorderStyle = System.Windows.Forms.BorderStyle.None;
+            this.richTextBox3.Location = new System.Drawing.Point(48, 307);
+            this.richTextBox3.Name = "richTextBox3";
+            this.richTextBox3.Size = new System.Drawing.Size(882, 100);
+            this.richTextBox3.TabIndex = 5;
+            this.richTextBox3.Text = "";
+            // 
+            // label6
+            // 
+            this.label6.AutoSize = true;
+            this.label6.Font = new System.Drawing.Font("Microsoft Sans Serif", 10.2F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label6.Location = new System.Drawing.Point(44, 3);
+            this.label6.Name = "label6";
+            this.label6.Size = new System.Drawing.Size(88, 20);
+            this.label6.TabIndex = 1;
+            this.label6.Text = "Checksum";
+            // 
+            // comboBox1
+            // 
+            this.comboBox1.BackColor = System.Drawing.Color.WhiteSmoke;
+            this.comboBox1.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.comboBox1.Font = new System.Drawing.Font("Microsoft Sans Serif", 10.2F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.comboBox1.FormattingEnabled = true;
+            this.comboBox1.Items.AddRange(new object[] {
+            "8-bit",
+            "16-bit",
+            "32-bit"});
+            this.comboBox1.Location = new System.Drawing.Point(48, 26);
+            this.comboBox1.Name = "comboBox1";
+            this.comboBox1.Size = new System.Drawing.Size(152, 28);
+            this.comboBox1.TabIndex = 2;
+            // 
+            // comboBox2
+            // 
+            this.comboBox2.BackColor = System.Drawing.Color.WhiteSmoke;
+            this.comboBox2.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.comboBox2.Font = new System.Drawing.Font("Microsoft Sans Serif", 10.2F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.comboBox2.FormattingEnabled = true;
+            this.comboBox2.Items.AddRange(new object[] {
+            "Sum",
+            "2\'s complement",
+            "Xor"});
+            this.comboBox2.Location = new System.Drawing.Point(218, 26);
+            this.comboBox2.Name = "comboBox2";
+            this.comboBox2.Size = new System.Drawing.Size(231, 28);
+            this.comboBox2.TabIndex = 2;
+            // 
+            // richTextBox4
+            // 
+            this.richTextBox4.Anchor = ((System.Windows.Forms.AnchorStyles)(((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Left) 
+            | System.Windows.Forms.AnchorStyles.Right)));
+            this.richTextBox4.BackColor = System.Drawing.Color.WhiteSmoke;
+            this.richTextBox4.BorderStyle = System.Windows.Forms.BorderStyle.None;
+            this.richTextBox4.Font = new System.Drawing.Font("Microsoft Sans Serif", 10.2F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.richTextBox4.Location = new System.Drawing.Point(483, 26);
+            this.richTextBox4.Multiline = false;
+            this.richTextBox4.Name = "richTextBox4";
+            this.richTextBox4.ReadOnly = true;
+            this.richTextBox4.Size = new System.Drawing.Size(447, 28);
+            this.richTextBox4.TabIndex = 4;
+            this.richTextBox4.Text = "";
             // 
             // UCBase64
             // 
-            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
-            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.Margin = new System.Windows.Forms.Padding(4, 4, 4, 4);
+            this.AutoScaleDimensions = new System.Drawing.SizeF(120F, 120F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Dpi;
+            this.Controls.Add(this.panelChecksum);
+            this.Controls.Add(this.panelOutput);
+            this.Controls.Add(this.panelInput);
+            this.Margin = new System.Windows.Forms.Padding(4);
             this.Name = "UCBase64";
             this.Size = new System.Drawing.Size(980, 829);
+            this.panelInput.ResumeLayout(false);
+            this.panelInput.PerformLayout();
+            this.panelOutput.ResumeLayout(false);
+            this.panelOutput.PerformLayout();
+            this.panelChecksum.ResumeLayout(false);
+            this.panelChecksum.PerformLayout();
             this.ResumeLayout(false);
 
         }
 
         #endregion
+
+        private System.Windows.Forms.Panel panelInput;
+        private System.Windows.Forms.Panel panelOutput;
+        private System.Windows.Forms.Panel panelChecksum;
+        private System.Windows.Forms.ComboBox comboxCharacterEncoding;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Button buttonOpenFile;
+        private System.Windows.Forms.RichTextBox richtextInput;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Button button2;
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.RichTextBox richTextBox1;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.RichTextBox richTextBox3;
+        private System.Windows.Forms.Label label5;
+        private System.Windows.Forms.RichTextBox richTextBox2;
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.ComboBox comboBox2;
+        private System.Windows.Forms.ComboBox comboBox1;
+        private System.Windows.Forms.Label label6;
+        private System.Windows.Forms.RichTextBox richTextBox4;
     }
 }
