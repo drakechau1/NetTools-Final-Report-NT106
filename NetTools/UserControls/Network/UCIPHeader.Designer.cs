71d98e347822ab7e9f06b9b458d7d071ac2e1e70
Đức Châu Minh <19521371@ms.uit.edu.vn>
2021-06-26 11:09:34 +0700

    Done UI

--- NetTools/UserControls/Network/UCIPHeader.Designer.cs
+++ NetTools/UserControls/Network/UCIPHeader.Designer.cs
@@ -113,7 +113,7 @@ namespace NetTools.UserControls.Network
             // richtextHTTPHeaders
             // 
             this.richtextHTTPHeaders.BackColor = System.Drawing.Color.WhiteSmoke;
-            this.richtextHTTPHeaders.BorderStyle = System.Windows.Forms.BorderStyle.None;
+            this.richtextHTTPHeaders.BorderStyle = System.Windows.Forms.BorderStyle.FixedSingle;
             this.richtextHTTPHeaders.Dock = System.Windows.Forms.DockStyle.Fill;
             this.richtextHTTPHeaders.Font = new System.Drawing.Font("Microsoft Sans Serif", 10.2F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
             this.richtextHTTPHeaders.Location = new System.Drawing.Point(50, 0);
--- NetTools/UserControls/Network/UCPing.Designer.cs
+++ NetTools/UserControls/Network/UCPing.Designer.cs
@@ -29,19 +29,120 @@ namespace NetTools.UserControls.Network
         /// </summary>
         private void InitializeComponent()
         {
+            this.panel1 = new System.Windows.Forms.Panel();
+            this.panel2 = new System.Windows.Forms.Panel();
+            this.label1 = new System.Windows.Forms.Label();
+            this.textDomain = new System.Windows.Forms.TextBox();
+            this.buttonGo = new System.Windows.Forms.Button();
+            this.label2 = new System.Windows.Forms.Label();
+            this.listboxPingInfo = new System.Windows.Forms.ListBox();
+            this.panel1.SuspendLayout();
+            this.panel2.SuspendLayout();
             this.SuspendLayout();
+            // 
+            // panel1
+            // 
+            this.panel1.Controls.Add(this.label2);
+            this.panel1.Controls.Add(this.buttonGo);
+            this.panel1.Controls.Add(this.textDomain);
+            this.panel1.Controls.Add(this.label1);
+            this.panel1.Dock = System.Windows.Forms.DockStyle.Top;
+            this.panel1.Font = new System.Drawing.Font("Microsoft Sans Serif", 10.2F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.panel1.Location = new System.Drawing.Point(0, 0);
+            this.panel1.Name = "panel1";
+            this.panel1.Padding = new System.Windows.Forms.Padding(50, 0, 50, 0);
+            this.panel1.Size = new System.Drawing.Size(980, 100);
+            this.panel1.TabIndex = 0;
+            // 
+            // panel2
+            // 
+            this.panel2.Controls.Add(this.listboxPingInfo);
+            this.panel2.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.panel2.Font = new System.Drawing.Font("Microsoft Sans Serif", 10.2F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.panel2.Location = new System.Drawing.Point(0, 100);
+            this.panel2.Name = "panel2";
+            this.panel2.Padding = new System.Windows.Forms.Padding(50, 0, 50, 30);
+            this.panel2.Size = new System.Drawing.Size(980, 729);
+            this.panel2.TabIndex = 1;
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Font = new System.Drawing.Font("Microsoft Sans Serif", 10.2F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label1.Location = new System.Drawing.Point(50, 30);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(149, 20);
+            this.label1.TabIndex = 0;
+            this.label1.Text = "Domain/IPAddress";
+            // 
+            // textDomain
+            // 
+            this.textDomain.Font = new System.Drawing.Font("Microsoft Sans Serif", 10.2F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.textDomain.Location = new System.Drawing.Point(205, 27);
+            this.textDomain.MinimumSize = new System.Drawing.Size(541, 27);
+            this.textDomain.Name = "textDomain";
+            this.textDomain.Size = new System.Drawing.Size(541, 27);
+            this.textDomain.TabIndex = 1;
+            // 
+            // buttonGo
+            // 
+            this.buttonGo.BackColor = System.Drawing.Color.WhiteSmoke;
+            this.buttonGo.FlatAppearance.BorderSize = 0;
+            this.buttonGo.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.buttonGo.Font = new System.Drawing.Font("Microsoft Sans Serif", 10.2F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.buttonGo.Location = new System.Drawing.Point(752, 22);
+            this.buttonGo.Name = "buttonGo";
+            this.buttonGo.Size = new System.Drawing.Size(75, 37);
+            this.buttonGo.TabIndex = 2;
+            this.buttonGo.Text = "Go";
+            this.buttonGo.UseVisualStyleBackColor = false;
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Font = new System.Drawing.Font("Microsoft Sans Serif", 9F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label2.Location = new System.Drawing.Point(202, 57);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(189, 18);
+            this.label2.TabIndex = 3;
+            this.label2.Text = "Example: www.google.com";
+            // 
+            // listboxPingInfo
+            // 
+            this.listboxPingInfo.BackColor = System.Drawing.Color.WhiteSmoke;
+            this.listboxPingInfo.BorderStyle = System.Windows.Forms.BorderStyle.FixedSingle;
+            this.listboxPingInfo.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.listboxPingInfo.FormattingEnabled = true;
+            this.listboxPingInfo.ItemHeight = 20;
+            this.listboxPingInfo.Location = new System.Drawing.Point(50, 0);
+            this.listboxPingInfo.Name = "listboxPingInfo";
+            this.listboxPingInfo.Size = new System.Drawing.Size(880, 699);
+            this.listboxPingInfo.TabIndex = 0;
             // 
             // UCPing
             // 
-            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
-            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.Margin = new System.Windows.Forms.Padding(4, 4, 4, 4);
+            this.AutoScaleDimensions = new System.Drawing.SizeF(120F, 120F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Dpi;
+            this.Controls.Add(this.panel2);
+            this.Controls.Add(this.panel1);
+            this.Margin = new System.Windows.Forms.Padding(4);
             this.Name = "UCPing";
             this.Size = new System.Drawing.Size(980, 829);
+            this.panel1.ResumeLayout(false);
+            this.panel1.PerformLayout();
+            this.panel2.ResumeLayout(false);
             this.ResumeLayout(false);
 
         }
 
         #endregion
+
+        private System.Windows.Forms.Panel panel1;
+        private System.Windows.Forms.Panel panel2;
+        private System.Windows.Forms.Button buttonGo;
+        private System.Windows.Forms.TextBox textDomain;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.ListBox listboxPingInfo;
     }
 }
