8bd8d94eb5273e2636676f956141e5b0cf5fecb3
Đức Châu Minh <19521371@ms.uit.edu.vn>
2021-06-27 01:37:48 +0700

    Done IP Geo Location

--- NetTools/UserControls/IPAddress/UCIPGeoLocation.Designer.cs
+++ NetTools/UserControls/IPAddress/UCIPGeoLocation.Designer.cs
@@ -28,18 +28,117 @@
         /// </summary>
         private void InitializeComponent()
         {
+            this.panelControl = new System.Windows.Forms.Panel();
+            this.buttonGo = new System.Windows.Forms.Button();
+            this.textDomain = new System.Windows.Forms.TextBox();
+            this.label2 = new System.Windows.Forms.Label();
+            this.label1 = new System.Windows.Forms.Label();
+            this.panelDesktop = new System.Windows.Forms.Panel();
+            this.listViewGeoLocationInfo = new System.Windows.Forms.ListView();
+            this.panelControl.SuspendLayout();
+            this.panelDesktop.SuspendLayout();
             this.SuspendLayout();
+            // 
+            // panelControl
+            // 
+            this.panelControl.Controls.Add(this.buttonGo);
+            this.panelControl.Controls.Add(this.textDomain);
+            this.panelControl.Controls.Add(this.label2);
+            this.panelControl.Controls.Add(this.label1);
+            this.panelControl.Dock = System.Windows.Forms.DockStyle.Top;
+            this.panelControl.Font = new System.Drawing.Font("Microsoft Sans Serif", 10.2F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.panelControl.Location = new System.Drawing.Point(0, 0);
+            this.panelControl.Name = "panelControl";
+            this.panelControl.Padding = new System.Windows.Forms.Padding(50, 0, 50, 0);
+            this.panelControl.Size = new System.Drawing.Size(980, 100);
+            this.panelControl.TabIndex = 0;
+            // 
+            // buttonGo
+            // 
+            this.buttonGo.Font = new System.Drawing.Font("Microsoft Sans Serif", 10.2F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.buttonGo.Location = new System.Drawing.Point(758, 22);
+            this.buttonGo.Name = "buttonGo";
+            this.buttonGo.Size = new System.Drawing.Size(75, 37);
+            this.buttonGo.TabIndex = 2;
+            this.buttonGo.Text = "Go";
+            this.buttonGo.UseVisualStyleBackColor = true;
+            this.buttonGo.Click += new System.EventHandler(this.buttonGo_Click);
+            // 
+            // textDomain
+            // 
+            this.textDomain.Font = new System.Drawing.Font("Microsoft Sans Serif", 10.2F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.textDomain.Location = new System.Drawing.Point(205, 27);
+            this.textDomain.Name = "textDomain";
+            this.textDomain.Size = new System.Drawing.Size(547, 27);
+            this.textDomain.TabIndex = 1;
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Font = new System.Drawing.Font("Microsoft Sans Serif", 9F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label2.Location = new System.Drawing.Point(202, 57);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(329, 18);
+            this.label2.TabIndex = 0;
+            this.label2.Text = "Geo Location information provided bu ip-api.com";
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Font = new System.Drawing.Font("Microsoft Sans Serif", 10.2F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label1.Location = new System.Drawing.Point(50, 30);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(149, 20);
+            this.label1.TabIndex = 0;
+            this.label1.Text = "Domain/IPAddress";
+            // 
+            // panelDesktop
+            // 
+            this.panelDesktop.Controls.Add(this.listViewGeoLocationInfo);
+            this.panelDesktop.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.panelDesktop.Font = new System.Drawing.Font("Microsoft Sans Serif", 10.2F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.panelDesktop.Location = new System.Drawing.Point(0, 100);
+            this.panelDesktop.Name = "panelDesktop";
+            this.panelDesktop.Padding = new System.Windows.Forms.Padding(50, 0, 50, 30);
+            this.panelDesktop.Size = new System.Drawing.Size(980, 730);
+            this.panelDesktop.TabIndex = 1;
+            // 
+            // listViewGeoLocationInfo
+            // 
+            this.listViewGeoLocationInfo.BackColor = System.Drawing.Color.WhiteSmoke;
+            this.listViewGeoLocationInfo.BorderStyle = System.Windows.Forms.BorderStyle.FixedSingle;
+            this.listViewGeoLocationInfo.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.listViewGeoLocationInfo.HideSelection = false;
+            this.listViewGeoLocationInfo.Location = new System.Drawing.Point(50, 0);
+            this.listViewGeoLocationInfo.Name = "listViewGeoLocationInfo";
+            this.listViewGeoLocationInfo.Size = new System.Drawing.Size(880, 700);
+            this.listViewGeoLocationInfo.TabIndex = 3;
+            this.listViewGeoLocationInfo.UseCompatibleStateImageBehavior = false;
             // 
             // UCIPGeoLocation
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(120F, 120F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Dpi;
+            this.Controls.Add(this.panelDesktop);
+            this.Controls.Add(this.panelControl);
             this.Name = "UCIPGeoLocation";
             this.Size = new System.Drawing.Size(980, 830);
+            this.Load += new System.EventHandler(this.UCIPGeoLocation_Load);
+            this.panelControl.ResumeLayout(false);
+            this.panelControl.PerformLayout();
+            this.panelDesktop.ResumeLayout(false);
             this.ResumeLayout(false);
 
         }
 
         #endregion
+
+        private System.Windows.Forms.Panel panelControl;
+        private System.Windows.Forms.Panel panelDesktop;
+        private System.Windows.Forms.Button buttonGo;
+        private System.Windows.Forms.TextBox textDomain;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.ListView listViewGeoLocationInfo;
     }
 }
--- NetTools/UserControls/IPAddress/UCIPGeoLocation.cs
+++ NetTools/UserControls/IPAddress/UCIPGeoLocation.cs
@@ -1,12 +1,8 @@
 ﻿using System;
-using System.Collections.Generic;
-using System.ComponentModel;
-using System.Data;
-using System.Drawing;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
+using System.IO;
+using System.Net;
 using System.Windows.Forms;
+using System.Xml;
 
 namespace NetTools.UserControls.IPAddress
 {
@@ -15,6 +11,86 @@ namespace NetTools.UserControls.IPAddress
         public UCIPGeoLocation()
         {
             InitializeComponent();
+        }
+
+
+        #region Methods
+        private string GetIPAddress(string url)
+        {
+            /* Check url is valid? */
+            if (!url.Contains("http"))
+                return url;
+            try
+            {
+                Uri myUri = new Uri(url);
+                string host = myUri.Host;
+                return host;
+            }
+            catch
+            {
+                return url;
+            }
+        }
+        private string GetHTTPRequest(string ipAddress)
+        {
+            string URL = $"http://ip-api.com/xml/{GetIPAddress(textDomain.Text)}?fields=status,message,continent,continentCode,country,countryCode,region,regionName,city,district,zip,lat,lon,timezone,offset,currency,isp,org,as,asname,reverse,mobile,proxy,hosting,query";
+            WebRequest webRequest = WebRequest.Create(URL);
+            webRequest.Credentials = CredentialCache.DefaultCredentials;
+            WebResponse webResponse = webRequest.GetResponse();
+            Stream stream = webResponse.GetResponseStream();
+            StreamReader streamReader = new StreamReader(stream);
+            string httpResponse = streamReader.ReadToEnd();
+            stream.Close();
+            webResponse.Close();
+            return httpResponse;
+        }
+        private bool CheckQueryReponse(XmlDocument xDoc)
+        {
+            if (xDoc.SelectSingleNode("//message") != null)
+                return false;
+            return true;
+        }
+        private void GetIpGeoLocationInfo(XmlDocument xDoc)
+        {
+            XmlElement xmlNodeList = xDoc.DocumentElement;
+            foreach (XmlNode element in xmlNodeList)
+            {
+                Console.WriteLine("{0}:{1}", element.Name, element.InnerText);
+                ListViewItem item = new ListViewItem(new string[] { element.Name, element.InnerText });
+                listViewGeoLocationInfo.Items.Add(item);
+            }
+        }
+        private void ResetListview()
+        {
+            listViewGeoLocationInfo.Items.Clear(); 
+        }
+        #endregion
+
+        private void UCIPGeoLocation_Load(object sender, EventArgs e)
+        {
+            /* Initialize listview */
+            listViewGeoLocationInfo.Columns.Add("Field", 200);
+            listViewGeoLocationInfo.Columns.Add("Value", listViewGeoLocationInfo.Width - 300);
+            listViewGeoLocationInfo.View = View.Details;
+        }
+        private void buttonGo_Click(object sender, EventArgs e)
+        {
+            string URL = textDomain.Text;
+            ResetListview();
+
+            string xmlQuery = GetHTTPRequest(URL);
+            XmlDocument xDoc = new XmlDocument();
+            xDoc.LoadXml(xmlQuery);
+
+            if (!CheckQueryReponse(xDoc))
+            {
+                /* Error response*/
+                ListViewItem item = new ListViewItem(new string[] { null, "Country Unable to resolve IP address, test stopped." });
+                listViewGeoLocationInfo.Items.Add(item);
+                return;
+            }
+            /* Success response */
+            GetIpGeoLocationInfo(xDoc);
         }
     }
 }
