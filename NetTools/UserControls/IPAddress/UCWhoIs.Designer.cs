8fe3b620c96733d5cdd2fb8331cc036194673d31
Đức Châu Minh <19521371@ms.uit.edu.vn>
2021-06-28 16:52:11 +0700

    Done base whois

--- NetTools/UserControls/IPAddress/UCWhoIs.Designer.cs
+++ NetTools/UserControls/IPAddress/UCWhoIs.Designer.cs
@@ -28,18 +28,119 @@
         /// </summary>
         private void InitializeComponent()
         {
+            this.panel1 = new System.Windows.Forms.Panel();
+            this.panel2 = new System.Windows.Forms.Panel();
+            this.labelDomain = new System.Windows.Forms.Label();
+            this.textDomain = new System.Windows.Forms.TextBox();
+            this.buttonGo = new System.Windows.Forms.Button();
+            this.richTextInformation = new System.Windows.Forms.RichTextBox();
+            this.label1 = new System.Windows.Forms.Label();
+            this.panel1.SuspendLayout();
+            this.panel2.SuspendLayout();
             this.SuspendLayout();
+            // 
+            // panel1
+            // 
+            this.panel1.Controls.Add(this.label1);
+            this.panel1.Controls.Add(this.buttonGo);
+            this.panel1.Controls.Add(this.textDomain);
+            this.panel1.Controls.Add(this.labelDomain);
+            this.panel1.Dock = System.Windows.Forms.DockStyle.Top;
+            this.panel1.Font = new System.Drawing.Font("Microsoft Sans Serif", 10.2F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.panel1.Location = new System.Drawing.Point(0, 0);
+            this.panel1.Name = "panel1";
+            this.panel1.Padding = new System.Windows.Forms.Padding(50, 0, 50, 0);
+            this.panel1.Size = new System.Drawing.Size(980, 100);
+            this.panel1.TabIndex = 0;
+            // 
+            // panel2
+            // 
+            this.panel2.Controls.Add(this.richTextInformation);
+            this.panel2.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.panel2.Font = new System.Drawing.Font("Microsoft Sans Serif", 10.2F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.panel2.Location = new System.Drawing.Point(0, 100);
+            this.panel2.Name = "panel2";
+            this.panel2.Padding = new System.Windows.Forms.Padding(50, 0, 50, 30);
+            this.panel2.Size = new System.Drawing.Size(980, 730);
+            this.panel2.TabIndex = 1;
+            // 
+            // labelDomain
+            // 
+            this.labelDomain.AutoSize = true;
+            this.labelDomain.Font = new System.Drawing.Font("Microsoft Sans Serif", 10.2F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.labelDomain.Location = new System.Drawing.Point(50, 30);
+            this.labelDomain.Name = "labelDomain";
+            this.labelDomain.Size = new System.Drawing.Size(149, 20);
+            this.labelDomain.TabIndex = 0;
+            this.labelDomain.Text = "Domain/IPAddress";
+            // 
+            // textDomain
+            // 
+            this.textDomain.BorderStyle = System.Windows.Forms.BorderStyle.FixedSingle;
+            this.textDomain.Font = new System.Drawing.Font("Microsoft Sans Serif", 10.2F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.textDomain.Location = new System.Drawing.Point(205, 27);
+            this.textDomain.Name = "textDomain";
+            this.textDomain.Size = new System.Drawing.Size(547, 27);
+            this.textDomain.TabIndex = 1;
+            // 
+            // buttonGo
+            // 
+            this.buttonGo.BackColor = System.Drawing.Color.WhiteSmoke;
+            this.buttonGo.FlatAppearance.BorderSize = 0;
+            this.buttonGo.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.buttonGo.Font = new System.Drawing.Font("Microsoft Sans Serif", 10.2F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.buttonGo.Location = new System.Drawing.Point(758, 22);
+            this.buttonGo.Name = "buttonGo";
+            this.buttonGo.Size = new System.Drawing.Size(75, 37);
+            this.buttonGo.TabIndex = 2;
+            this.buttonGo.Text = "Go";
+            this.buttonGo.UseVisualStyleBackColor = false;
+            this.buttonGo.Click += new System.EventHandler(this.buttonGo_Click);
+            // 
+            // richTextInformation
+            // 
+            this.richTextInformation.BackColor = System.Drawing.Color.WhiteSmoke;
+            this.richTextInformation.BorderStyle = System.Windows.Forms.BorderStyle.FixedSingle;
+            this.richTextInformation.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.richTextInformation.Location = new System.Drawing.Point(50, 0);
+            this.richTextInformation.Name = "richTextInformation";
+            this.richTextInformation.Size = new System.Drawing.Size(880, 700);
+            this.richTextInformation.TabIndex = 0;
+            this.richTextInformation.Text = "";
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Font = new System.Drawing.Font("Microsoft Sans Serif", 9F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label1.Location = new System.Drawing.Point(201, 57);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(333, 18);
+            this.label1.TabIndex = 3;
+            this.label1.Text = "The information provided by whois.whoisxml.com";
             // 
             // UCWhoIs
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(120F, 120F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Dpi;
+            this.Controls.Add(this.panel2);
+            this.Controls.Add(this.panel1);
             this.Name = "UCWhoIs";
             this.Size = new System.Drawing.Size(980, 830);
+            this.panel1.ResumeLayout(false);
+            this.panel1.PerformLayout();
+            this.panel2.ResumeLayout(false);
             this.ResumeLayout(false);
 
         }
 
         #endregion
+
+        private System.Windows.Forms.Panel panel1;
+        private System.Windows.Forms.Panel panel2;
+        private System.Windows.Forms.Button buttonGo;
+        private System.Windows.Forms.TextBox textDomain;
+        private System.Windows.Forms.Label labelDomain;
+        private System.Windows.Forms.RichTextBox richTextInformation;
+        private System.Windows.Forms.Label label1;
     }
 }
--- NetTools/UserControls/IPAddress/UCWhoIs.cs
+++ NetTools/UserControls/IPAddress/UCWhoIs.cs
@@ -3,10 +3,13 @@ using System.Collections.Generic;
 using System.ComponentModel;
 using System.Data;
 using System.Drawing;
+using System.IO;
 using System.Linq;
+using System.Net;
 using System.Text;
 using System.Threading.Tasks;
 using System.Windows.Forms;
+using System.Xml;
 
 namespace NetTools.UserControls.IPAddress
 {
@@ -15,6 +18,175 @@ namespace NetTools.UserControls.IPAddress
         public UCWhoIs()
         {
             InitializeComponent();
+        }
+
+        #region Methods
+        private void ResetRichText()
+        {
+            richTextInformation.Clear();
+        }
+        private string VerifyDomain(string url)
+        {
+            /* Check url is valid? */
+            if (!url.Contains("http"))
+                return url;
+            try
+            {
+                Uri myUri = new Uri(url);
+                string host = myUri.Host;
+                return host;
+            }
+            catch
+            {
+                return url;
+            }
+        }
+        private string GetHTTPRequest(string domain)
+        {
+            /* Verified domain */
+            domain = VerifyDomain(domain);
+            /* Get api */
+            string URL = $"https://www.whoisxmlapi.com/whoisserver/WhoisService?apiKey=at_aVzDK57jaecAL81LDLNTpFpZobX4B&domainName={domain}";
+            WebRequest webRequest = WebRequest.Create(URL);
+            webRequest.Credentials = CredentialCache.DefaultCredentials;
+            WebResponse webResponse = webRequest.GetResponse();
+            Stream stream = webResponse.GetResponseStream();
+            StreamReader streamReader = new StreamReader(stream);
+            string httpResponse = streamReader.ReadToEnd();
+            stream.Close();
+            webResponse.Close();
+            return httpResponse;
+        }
+        private string CheckQueryReponse(XmlDocument xDoc)
+        {
+            if (xDoc.SelectSingleNode("//errorCode") != null)
+            {
+                string errorCode = $"Error code: {xDoc.SelectSingleNode("//errorCode").InnerText}";
+                string msg = $"Message: {xDoc.SelectSingleNode("//msg").InnerText}";
+                return errorCode + "\n" + msg;
+            }
+            if (xDoc.SelectSingleNode("//dataError") != null)
+            {
+                return xDoc.SelectSingleNode("//dataError").InnerText;
+            }
+            return null;
+        }
+        #endregion
+
+        #region Detail information
+        private string GetRegistrant(XmlDocument xDoc)
+        {
+            try
+            {
+                string info = xDoc.SelectSingleNode("//registrant/rawText").InnerText;
+                return info;
+            }
+            catch (Exception ex)
+            {
+                return ex.Message;
+            }
+        }
+        private string GetAdministrativeContact(XmlDocument xDoc)
+        {
+            try
+            {
+                string info = xDoc.SelectSingleNode("//administrativeContact/rawText").InnerText;
+                return info;
+            }
+            catch (Exception ex)
+            {
+                return ex.Message;
+            }
+        }
+        private string GetTechnicalContact(XmlDocument xDoc)
+        {
+            try
+            {
+                string info = xDoc.SelectSingleNode("//technicalContact/rawText").InnerText;
+                return info;
+            }
+            catch (Exception ex)
+            {
+                return ex.Message;
+            }
+        }
+        private string GetRecordUpdate(XmlDocument xDoc)
+        {
+            try
+            {
+                string createdDate = xDoc.SelectSingleNode("//audit/createdDate").InnerText;
+                string updatedDate = xDoc.SelectSingleNode("//audit/updatedDate").InnerText;
+                return $"Created date: {createdDate}\nUpdated date: {updatedDate}";
+            }
+            catch (Exception ex)
+            {
+                return ex.Message;
+            }
+        }
+        private string GetNameServers(XmlDocument xDoc)
+        {
+            try
+            {
+                string info = xDoc.SelectSingleNode("//nameServers/rawText").InnerText;
+                return $"Host names:\n{info}";
+            }
+            catch (Exception ex)
+            {
+                return ex.Message;
+            }
+        }
+        private string GetRegistryData(XmlDocument xDoc)
+        {
+            try
+            {
+                string info = null;
+                info = $"Created date: {xDoc.SelectSingleNode("//registryData/createdDate").InnerText}" +
+                    $"\nUpdated date: {xDoc.SelectSingleNode("//registryData/updatedDate").InnerText}" +
+                    $"\nExpires date: {xDoc.SelectSingleNode("//registryData/expiresDate").InnerText}" +
+                    $"\nParsed domain name: {xDoc.SelectSingleNode("//registryData/domainName").InnerText}" +
+                    $"\n\nHost names \n{xDoc.SelectSingleNode("//registryData/nameServers/rawText").InnerText}" +
+                    $"\nDomain EPP status codes by ICANN list: {xDoc.SelectSingleNode("//registryData/status").InnerText}" +
+                    $"\n\nRecord update dates: " +
+                    $"\nCreated date: {xDoc.SelectSingleNode("//registryData/audit/createdDate").InnerText}" +
+                    $"\nUpdated date: {xDoc.SelectSingleNode("//registryData/audit/updatedDate").InnerText}" +
+                    $"";
+                return info;
+            }
+            catch (Exception ex)
+            {
+                return ex.Message;
+            }
+        }
+        private string GetWhoIsInformation(XmlDocument xDoc)
+        {
+            if (xDoc == null)
+                return null;
+            string error = CheckQueryReponse(xDoc);
+            if (error != null)
+                return error;
+
+            string whoIsInformation = null;
+            whoIsInformation = $"*** RECORD UPDATE DATES *** \n{GetRecordUpdate(xDoc)}" +
+                $"\n\n*** REGISTRANT *** \n{GetRegistrant(xDoc)}" +
+                $"\n\n *** ADMINISTRATIVE CONTACT *** \n{GetAdministrativeContact(xDoc)}" +
+                $"\n\n *** TECHNICAL CONTACT *** \n{GetTechnicalContact(xDoc)}" +
+                $"\n\n *** NAME SERVERS *** \n{GetNameServers(xDoc)}" +
+                $"\n *** REGISTRY DATA *** \n{GetRegistryData(xDoc)}";
+
+            return whoIsInformation;
+        }
+        #endregion
+
+        private void buttonGo_Click(object sender, EventArgs e)
+        {
+            if (textDomain.Text != string.Empty)
+            {
+                string xmlRespone = GetHTTPRequest(textDomain.Text);
+                XmlDocument xDoc = new XmlDocument();
+                xDoc.LoadXml(xmlRespone);
+                ResetRichText();
+                richTextInformation.Text = GetWhoIsInformation(xDoc);
+            }
         }
     }
 }
