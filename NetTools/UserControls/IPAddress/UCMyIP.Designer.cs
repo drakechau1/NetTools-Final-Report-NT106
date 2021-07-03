e52ed4f659b7dc04011246d09a5e2b235eaced99
Đức Châu Minh <19521371@ms.uit.edu.vn>
2021-07-03 17:55:10 +0700

    Done MyIP

--- NetTools/UserControls/IPAddress/UCMyIP.Designer.cs
+++ NetTools/UserControls/IPAddress/UCMyIP.Designer.cs
@@ -29,18 +29,51 @@ namespace NetTools.UserControls.IPAddress
         /// </summary>
         private void InitializeComponent()
         {
+            this.buttonRefresh = new System.Windows.Forms.Button();
+            this.richMyIP = new System.Windows.Forms.RichTextBox();
             this.SuspendLayout();
+            // 
+            // buttonRefresh
+            // 
+            this.buttonRefresh.Font = new System.Drawing.Font("Microsoft Sans Serif", 10.2F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.buttonRefresh.Location = new System.Drawing.Point(50, 30);
+            this.buttonRefresh.Name = "buttonRefresh";
+            this.buttonRefresh.Size = new System.Drawing.Size(107, 37);
+            this.buttonRefresh.TabIndex = 0;
+            this.buttonRefresh.Text = "Refresh";
+            this.buttonRefresh.UseVisualStyleBackColor = true;
+            this.buttonRefresh.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // richMyIP
+            // 
+            this.richMyIP.Anchor = ((System.Windows.Forms.AnchorStyles)((((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Bottom) 
+            | System.Windows.Forms.AnchorStyles.Left) 
+            | System.Windows.Forms.AnchorStyles.Right)));
+            this.richMyIP.BackColor = System.Drawing.Color.WhiteSmoke;
+            this.richMyIP.Font = new System.Drawing.Font("Microsoft Sans Serif", 10.2F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.richMyIP.Location = new System.Drawing.Point(53, 94);
+            this.richMyIP.Name = "richMyIP";
+            this.richMyIP.ReadOnly = true;
+            this.richMyIP.Size = new System.Drawing.Size(874, 703);
+            this.richMyIP.TabIndex = 1;
+            this.richMyIP.Text = "";
             // 
             // UCMyIP
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(120F, 120F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Dpi;
+            this.Controls.Add(this.richMyIP);
+            this.Controls.Add(this.buttonRefresh);
             this.Name = "UCMyIP";
+            this.Padding = new System.Windows.Forms.Padding(50, 0, 50, 30);
             this.Size = new System.Drawing.Size(980, 830);
             this.ResumeLayout(false);
 
         }
 
         #endregion
+
+        private System.Windows.Forms.Button buttonRefresh;
+        private System.Windows.Forms.RichTextBox richMyIP;
     }
 }
